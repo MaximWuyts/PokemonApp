8a7aa281e9c9221a00bfacfe868d6ea218ced27f
MaximWuyts <32163906+MaximWuyts@users.noreply.github.com>
2018-08-16 14:05:16 +0200

    loading

--- pokemon_app/src/Components/_pages/PokemonOverview.Component.js
+++ pokemon_app/src/Components/_pages/PokemonOverview.Component.js
@@ -25,12 +25,29 @@ export class PokemonOverview extends React.Component {
     return this.state.pokemons.map((pokemon, i) => <Pokemon key={i} name={pokemon.name} />);
   };
 
+
+
   render = () => {
+
+    var overlay = document.getElementById("overlay");
+    window.addEventListener('load', function () {
+
+    })
+
     return (
       <div>
+        <div id="overlay">
+          <div class="loader">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
         <Link to="/"><img alt="header" className="header_img" src={header}></img></Link>
         <ThirdButton url="/add-pokemon" name="Catch A Pokemon" />
+        {setTimeout(function () { alert("Hello"); }, 3000)};
         {this.renderPokemons()}
+
 
       </div>
     );
